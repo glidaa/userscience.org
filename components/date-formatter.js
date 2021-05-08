afe11c97ed07de288a94bfe8f29e37c4fdc28eef
agris <agris.melko@gmail.com>
2021-05-08 09:22:48 +0300

    Date formating, image formating, opt data check

--- components/date-formatter.js
+++ components/date-formatter.js
@@ -1,6 +1,6 @@
 import { parseISO, format } from 'date-fns'
 
 export default function DateFormatter({ dateString }) {
-  const date = parseISO(dateString)
+  const date = parseISO(dateString.replace("'","").replace("'",""));
   return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
 }
